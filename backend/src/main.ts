88f4a57e971fcd250234bf6fd8ea8b579ed89ebd
= <sahil@tabifyapp.com>
2020-09-23 17:23:46 -0400

    enabled cors - whitelisted frontend ip'

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    cors: false,
+    cors: {
+      origin: ['http://localhost:4200'],
+    },
   });
   await app.listen(3000);
 }
