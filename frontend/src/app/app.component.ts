2e23319946ca2ff0d53604fc271138ff8a610135
K. J. Justice <k.j.justice@K-Js-MBP.hsd1.ma.comcast.net>
2020-09-23 21:03:45 -0400

    Made changes to the post reequest.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -22,7 +22,7 @@ export class AppComponent {
   async postUsers(){
     const userToAdd = { companyId: 12, companyName: "StormBreaker Enterprise", email: "MaryLan@nightwellenterprise.com", employeeId: 3, firstName: "Mary", isManager: true, lastName: "Lan", managerId: 1, password: "pwd", positionTitle: "Engineering Manager", startDate: "2016-07-22T00:00:00.000Z", __v: 0, _id: "5ts02e32e5d88f46a442ce09"}
     const result = await this.httpClient.post('http://localhost:3000/user', userToAdd);
-    console.log(result);
+    //console.log(result);
   }
 
 }
