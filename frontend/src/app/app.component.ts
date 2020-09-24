9333d81802ba1779f85c68b24c1c03ef409276e5
K. J. Justice <k.j.justice@K-Js-MBP.hsd1.ma.comcast.net>
2020-09-23 21:28:37 -0400

    Changes to post request

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -19,9 +19,9 @@ export class AppComponent {
     console.log(result);
   }
 
-  async postUsers(): Promise<void>{
+  async postUsers(){
     const userToAdd = { companyId: 12, companyName: "StormBreaker Enterprise", email: "MaryLan@nightwellenterprise.com", employeeId: 3, firstName: "Mary", isManager: true, lastName: "Lan", managerId: 1, password: "pwd", positionTitle: "Engineering Manager", startDate: "2016-07-22T00:00:00.000Z", __v: 0, _id: "5ts02e32e5d88f46a442ce09"}
-    const result = await this.httpClient.post('http://localhost:3000/user', userToAdd).toPromise();
+    const result = await this.httpClient.post('http://localhost:3000/user', userToAdd);
     console.log(result);
   }
 
