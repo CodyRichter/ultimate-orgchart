b205de4a20ded18a4b5ce9e0cf152a04d3a42ebf
Qiming-C <49624964+Qiming-C@users.noreply.github.com>
2020-09-28 22:49:22 -0400

    controller-scoped guard

--- backend/src/employee/employee.controller.ts
+++ backend/src/employee/employee.controller.ts
@@ -1,8 +1,11 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body,UseGuards,Request } from '@nestjs/common';
 import { EmployeeService } from './employee.service';
 import { Employee } from './employee.model';
+import {JwtAuthGuard} from '../auth/guards/jwt-auth.guard';
 
+//this is controller-scoped guard which guarantee the endpoint is protected 
 @Controller("employee")
+@UseGuards(JwtAuthGuard)
 export class EmployeeController {
   constructor(private readonly employeeService: EmployeeService) {}
 
@@ -14,5 +17,11 @@ export class EmployeeController {
   @Post()
   async createEmployee(@Body() newEmployee: Employee): Promise<void> {
     return await this.employeeService.createEmployee(newEmployee);
+  }
+
+  @Get('profile')
+  getProfile(@Request() req)
+  {
+      return req.user;
   }
 }
