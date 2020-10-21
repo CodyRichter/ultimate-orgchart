a4b1f65b03dc1e4d454be7dee46e3bae4c83cf45
Qiming-C <49624964+Qiming-C@users.noreply.github.com>
2020-10-20 21:28:28 -0400

    create and get endpoints

--- backend/src/project/project.controller.ts
+++ backend/src/project/project.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { ProjectService } from './project.service';
 
 @Controller('project')
-export class ProjectController {}
+export class ProjectController { 
+    constructor(private readonly projectService:ProjectService){}
+
+
+    @Post('create')
+    async createProject()
+
+
+
+    @Get(':projectId')
+    async getProject()
+
+
+    
+
+
+
+
+
+}
+
+
+
+
+
+
