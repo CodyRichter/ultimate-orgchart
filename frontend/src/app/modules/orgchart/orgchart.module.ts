b7a4f82a33e6babae655525c0673e318e5dd0147
Jinyuan Huang <jinyuan@jinyuanhuang.com>
2020-10-26 07:49:29 -0400

    introduce new chart components

--- frontend/src/app/modules/orgchart/orgchart.module.ts
+++ frontend/src/app/modules/orgchart/orgchart.module.ts
@@ -6,9 +6,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NodeSelectService } from './shared/services/node-select.service';
 import { MatCardModule } from '@angular/material/card';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { ChartStackComponent } from './chart-stack/chart-stack.component';
+import { ChartManagerComponent } from './chart-manager/chart-manager.component';
+import { ChartEmployeeComponent } from './chart-employee/chart-employee.component';
+import { TestContainerComponent } from './test-container/test-container.component';
+import { ChartRootComponent } from './chart-root/chart-root.component';
 
 @NgModule({
-  declarations: [ChartContainerComponent, ChartNodeComponent],
+  declarations: [ChartContainerComponent, ChartNodeComponent, ChartStackComponent, ChartManagerComponent, ChartEmployeeComponent, TestContainerComponent, ChartRootComponent],
   exports: [
     ChartContainerComponent,
     ChartNodeComponent
