4568c39b2504781cdb96b65742f446d59243bd2b
Jinyuan Huang <jinyuan@jinyuanhuang.com>
2020-10-26 23:53:42 -0400

    reuse chart node component

---
+++ frontend/src/app/modules/orgchart/chart-node/chart-node.component.ts
@@ -0,0 +1,33 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'chart-node',
+  templateUrl: './chart-node.component.html',
+  styleUrls: ['./chart-node.component.css']
+})
+export class ChartNodeComponent implements OnInit {
+
+  @Input() nodeData;
+
+  @Output() nodeClick = new EventEmitter<any>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getColor(pos): string {
+    const color =
+        {
+          'Engineering Manager': '#FFBA00',
+          CEO: '#3C9329',
+          'Software Engineer II': '#0093FF',
+          'Tech Lead': '#019592',
+          'Software Engineer I': '#7F39FB',
+          'Research Manager': '#E57373',
+          'Software Architect': '#00BCD4'
+        };
+    return 'background: linear-gradient(0deg, white 80%,' + color[pos] +  ' 20%);';
+  }
+
+}
--- frontend/src/app/modules/orgchart/chart-stack/chart-stack.component.ts
+++ frontend/src/app/modules/orgchart/chart-stack/chart-stack.component.ts
@@ -7,41 +7,11 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class ChartStackComponent implements OnInit {
 
-    @Input() datasource;
-
-  nodeData = {
-    name: 'Test Test',
-    title: 'Software Engineer II'
-  };
-  nodeData1 = {
-    name: 'Test Test',
-    title: 'Engineering Manager'
-  };
-  nodeData2 = {
-    name: 'Test Test',
-    title: 'Software Engineer I'
-  };
+  @Input() datasource;
 
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  getColor(pos): string {
-    const color =
-        {
-          'Engineering Manager': '#FFBA00',
-          CEO: '#3C9329',
-          'Software Engineer II': '#0093FF',
-          'Tech Lead': '#019592',
-          'Software Engineer I': '#7F39FB',
-          'Research Manager': '#E57373',
-          'Software Architect': '#00BCD4'
-        };
-    return 'background: linear-gradient(0deg, white 80%,' + color[pos] +  ' 20%);';
-  }
-
-  onStackClick(): void {
   }
 
 }
