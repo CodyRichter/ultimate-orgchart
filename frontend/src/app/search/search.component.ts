3ca350023bbaf6f62499b6ed632c61cd49d0d037
K. J. Justice <k.j.justice@K-Js-MBP.hsd1.ma.comcast.net>
2020-10-28 16:22:12 -0400

    added list of employees

--- frontend/src/app/search/search.component.ts
+++ frontend/src/app/search/search.component.ts
@@ -32,8 +32,11 @@ export class SearchComponent implements OnInit {
 
   async myFunction():Promise<void>{
     var currentVal = (<HTMLInputElement>document.getElementById("mySearch")).value;
+    var dialoginput = (<HTMLInputElement>document.getElementById("dialog"))
     var input = (await this.employeeService.searchEmployee(currentVal));
-    console.log(input[0].positionTitle);
+    for(var i = 0; i<input.length;++i){
+      dialoginput.innerHTML = `<a mat-list-item><span mat-line>${input[i].firstName} ${input[i].lastName}<br></span></a>`;
+    }
     console.log(input);
     return input;
   }
