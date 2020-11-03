7a96d017861eee12d8a764bc24049b4d630cff83
K. J. Justice <k.j.justice@K-Js-MBP.hsd1.ma.comcast.net>
2020-11-03 13:55:12 -0500

    search feature code cleanup

--- frontend/src/app/search/search.component.ts
+++ frontend/src/app/search/search.component.ts
@@ -38,40 +38,4 @@ export class SearchComponent implements OnInit {
     var currentVal = (<HTMLInputElement>document.getElementById("mySearch")).value;
     return currentVal;
   }
-
-  async numberOfResults():Promise<void>{
-    var currentVal = (<HTMLInputElement>document.getElementById("mySearch")).value;
-    var input = (await this.employeeService.searchEmployee(currentVal))
-    var peopleHeader = (<HTMLInputElement>document.getElementById("people"))
-    let count = 0;
-    for(var i = 0; i<input.length;++i){ 
-      count=count+1;
-    }
-    let externalHTML = `<div style="margin-top:20px;"><h3>`
-    let extremeHTML = `</h3></div>`
-    let peopleHeaderText = "People: " + count + " Results"
-    peopleHeader.innerHTML = externalHTML + peopleHeaderText + extremeHTML;
-  }
-
-  async myFunction():Promise<void>{
-    this.numberOfResults();
-    var nodeData = []; //create an array
-    var currentVal = (<HTMLInputElement>document.getElementById("mySearch")).value; //value of search query
-    var dialoginput = (<HTMLInputElement>document.getElementById("dialog"))
-    var input = (await this.employeeService.searchEmployee(currentVal)) //filtered array
-    for(var i = 0; i<input.length;++i){ 
-        nodeData.push(input[i]); //push filtered results into nodeData array
-        console.log(nodeData);
-    let externalHTML = `<mat-list-item id="listItem"><div style="display: inline-block; padding-right:10px;"><img src="assets/icons/default-avatar.png" alt="" style="width: 40px"></div>
-    <div style="font-size: medium; display: inline-block; padding-right:10px;">
-        <strong>`;
-    let extremeHTML = `</strong>
-        </div></div>
-        <button *ngIf="node.manages.length > 0" mat-stroked-button class="button" color="warn" mat-dialog-close (click)="onNavigateClick(node)">Navigate</button></mat-list-item>`;
-    let employeeDetails = input[i].firstName + " " + input[i].lastName + "<br>" + input[i].positionTitle +  "<br>";
-    dialoginput.innerHTML += externalHTML + employeeDetails + extremeHTML + "<br>" + "<br>";
-    }
-    
-
-}
 }
