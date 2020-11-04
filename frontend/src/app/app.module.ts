6d5c8e95d6dc243607f6f16f18fdeaa1ef024f62
= <sahil@tabifyapp.com>
2020-11-04 15:22:22 -0500

    moved Search component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -27,7 +27,7 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
 import { AuthInterceptorService } from './services/auth/auth-interceptor.service';
 import { FileUploadComponent } from './components/file-upload/file-upload.component';
 import { EmployeeTransferComponent } from './components/employee-transfer/employee-transfer.component';
-import { SearchComponent } from './search/search.component';
+import { SearchComponent } from './components/search/search.component';
 import { OrgchartModule } from './modules/orgchart/orgchart.module';
 import { NotificationCardComponent } from './components/notification-card/notification-card.component';
 
