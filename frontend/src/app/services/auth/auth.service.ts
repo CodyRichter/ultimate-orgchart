c39d475ea6a9145fea5ec49634225b4b0671d704
= <sahil@tabifyapp.com>
2020-11-04 15:10:39 -0500

    updated interfaces to be local to frontend

--- frontend/src/app/services/auth/auth.service.ts
+++ frontend/src/app/services/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Employee } from '../../../../../backend/src/employee/employee.model';
+import { Employee } from 'src/app/models';
 
 @Injectable({
   providedIn: 'root'
