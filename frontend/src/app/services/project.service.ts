23295e1862fdf301fb66772cc134ed147140c589
Nikhil Devadoss <Nikhil@Nirajs-Air.hsd1.ca.comcast.net>
2020-11-11 00:18:03 -0800

    create project service created

--- frontend/src/app/services/project.service.ts
+++ frontend/src/app/services/project.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Project } from '../models';
-
+import {ProjectsEmployee } from '../models';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,6 +12,12 @@ export class ProjectService {
   async createProject(project: Project): Promise<Project> {
     return await this.httpClient.post('http://localhost:3000/project/create', project).toPromise() as Project;
   }
+
+  async addProjectEmployee(projectId: number, projectEmployees: ProjectsEmployee[]): Promise<void>{
+    await this.httpClient.patch('http://localhost:3000/project/addEmployee' + projectId, projectEmployees).toPromise();
+  }
 }
+
+
 
 
