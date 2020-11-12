18f81e3a4b7243a93c28bfd9c14f2f73d2c22ce3
rishab <rishabmahesh@umass.edu>
2020-11-11 20:59:27 -0600

    in progress

--- frontend/src/app/components/create-employee/create-employee.component.ts
+++ frontend/src/app/components/create-employee/create-employee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'create-employee',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateEmployeeComponent implements OnInit {
 
+  id = new FormControl('', [Validators.required])
+  email = new FormControl('', [Validators.required, Validators.email])
   constructor() { }
+
+  newEmployee: FormGroup;
 
   ngOnInit(): void {
   }
 
-}
+  
