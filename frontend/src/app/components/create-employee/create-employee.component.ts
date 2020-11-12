d2a4ccab9fa30b2dbe89e53b0e77d0396e8495a1
rishab <rishabmahesh@umass.edu>
2020-11-11 21:18:05 -0600

    "test"i

---
+++ frontend/src/app/components/create-employee/create-employee.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'create-employee',
+  templateUrl: './create-employee.component.html',
+  styleUrls: ['./create-employee.component.css']
+})
+export class CreateEmployeeComponent implements OnInit {
+
+  id = new FormControl('', [Validators.required]);
+  email = new FormControl('', [Validators.required, Validators.email])
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getEmailErrorMessage() {
+    if(this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+    return this.email.hasError('email')? 'Not a valid email': '';
+  }
+
+  getErrorMessage() {
+    if (this.id.hasError('required')) {
+      return 'You must enter a value';
+    }
+    return this.id.hasError('required')? 'You must nter a value':'';
+  }
+
+}
