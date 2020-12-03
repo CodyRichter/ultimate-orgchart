2f7d1411861f73d3f7837ccb5ebd0674a4283515
= <sahil@tabifyapp.com>
2020-12-02 22:09:11 -0500

    uncommented

--- frontend/src/app/components/notification-card/notification-card.component.ts
+++ frontend/src/app/components/notification-card/notification-card.component.ts
@@ -27,17 +27,17 @@ export class NotificationCardComponent implements OnInit {
 
   async approveRequest() {
     this.dismiss();
-    // console.log(await this.managerService.approveRequest((this.notification.managerRequest as ManagerRequest)._id));
+    console.log(await this.managerService.approveRequest((this.notification.managerRequest as ManagerRequest)._id));
   }
 
   async rejectRequest() {
     this.dismiss();
-    // console.log(await this.managerService.rejectRequest((this.notification.managerRequest as ManagerRequest)._id));
+    console.log(await this.managerService.rejectRequest((this.notification.managerRequest as ManagerRequest)._id));
   }
 
   async cancelRequest() {
     this.dismiss();
-    // console.log(await this.managerService.cancelRequest((this.notification.managerRequest as ManagerRequest)._id));
+    console.log(await this.managerService.cancelRequest((this.notification.managerRequest as ManagerRequest)._id));
   }
 
   async dismiss() {
